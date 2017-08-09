9f41324ac3c2ef5a18a04cc29f76efec284f71d5
Sushil Baid <sbaid@microsoft.com>
2017-08-09 19:19:54 +0530

    xunit 21 10k tests solution

---
+++ xunit21tests-10k/xunit21tests-10k/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xunit21tests_10k
+{
+    public class Class1
+    {
+    }
+}
