ceb6ea9b81edf5c2ed2e432ad2a4f0d1c24a88db
Sushil Baid <sbaid@microsoft.com>
2017-09-06 15:47:23 +0530

    datasource issue

---
+++ bugs/DataSourceItem/DataSourceItem/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataSourceItem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataSourceItem")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3bb544a0-fc97-47b6-b8f9-4366a468cd13")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ bugs/DataSourceItem/DataSourceItem/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataSourceItem
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        [DeploymentItem(@"XMLFile1.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"|DataDirectory|\XMLFile1.xml", "TestData", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var myData = TestContext.DataRow["MyColumn"].ToString();
+            Assert.IsTrue(myData.Contains("\r"), "XML data does not contain expected carriage-return character");
+        }
+
+    }
+}
