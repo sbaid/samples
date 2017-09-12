a3590c2ec30e938964904e84dc6bc355ded4221f
Sushil Baid <sbaid@microsoft.com>
2017-09-12 10:51:39 +0530

    nunitfx3.5 test

---
+++ NunitTestFx35/NunitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class Class1
+
+    {
+        [Test]
+        public void ProcessNameTest()
+
+        {
+            Assert.AreEqual("vstest.executionengine.x86.clr20", Process.GetCurrentProcess().ProcessName);
+        }
+
+        [Test]
+        public void ImageRuntimeVersionTest()
+        {
+            Assert.AreEqual("v2.0.50727", typeof(object).Assembly.ImageRuntimeVersion);
+        }
+    }
+}
