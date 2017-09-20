0a9108359e71583aa04ab8da835824e50d6e59cf
Sushil Baid <sbaid@microsoft.com>
2017-09-20 16:47:54 +0530

    data driven test with csv

---
+++ DataSourceTest-mstestv2/DataSourceTest-mstestv2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataSourceTest-mstestv2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataSourceTest-mstestv2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bdc1167-249a-47e0-8943-1316287d9ee3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DataSourceTest-mstestv2/DataSourceTest-mstestv2/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataSourceTest_mstestv2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext
+        {
+            get; set;
+        }
+
+        [DeploymentItem("a.csv")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "a.csv", "a#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var item1 = this.TestContext.DataRow["Item1"];
+            var item2 = this.TestContext.DataRow["Item2"];
+            var item3 = this.TestContext.DataRow[2];
+        }
+    }
+}
