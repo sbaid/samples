cf4773439a74946b43f368e77212d6a56af7e2f0
Sushil Baid <sbaid@microsoft.com>
2017-09-20 17:19:40 +0530

    xml data source

--- DataSourceTest-mstestv2/DataSourceTest-mstestv2/UnitTest1.cs
+++ DataSourceTest-mstestv2/DataSourceTest-mstestv2/UnitTest1.cs
@@ -14,11 +14,22 @@ namespace DataSourceTest_mstestv2
         [DeploymentItem("a.csv")]
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "a.csv", "a#csv", DataAccessMethod.Sequential)]
         [TestMethod]
-        public void TestMethod1()
+        public void TestWithCsvData()
         {
             var item1 = this.TestContext.DataRow["Item1"];
             var item2 = this.TestContext.DataRow["Item2"];
             var item3 = this.TestContext.DataRow[2];
+        }
+
+        [DeploymentItem("data.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "data.xml", "Row", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void TestWithXmlData()
+        {
+            var value1 = Convert.ToInt32(this.TestContext.DataRow["Value1"]);
+            var value2 = Convert.ToInt32(this.TestContext.DataRow["Value2"]);
+            var expectedValue = Convert.ToInt32(this.TestContext.DataRow["ExpectedValue"]);
+            Assert.AreEqual(value1 + value2, expectedValue);
         }
     }
 }
